266ef8f25f2463206c00547186175e1b8b012530
awyaki <awyakitaka@gmail.com>
2023-04-29 14:22:21 +0900

    chore: add story for three items and four items of List

--- src/components/list/List/List.stories.tsx
+++ src/components/list/List/List.stories.tsx
@@ -1,9 +1,25 @@
 import { type Meta, StoryObj } from "@storybook/react";
 import { List } from "./index";
 
+const dataForList: Parameters<typeof List>["0"]["data"] = [
+  { id: "1", text: "1日後" },
+  { id: "2", text: "2日後" },
+  { id: "3", text: "3日後" },
+  { id: "4", text: "4日後" },
+];
+
 const meta: Meta<typeof List> = {
   title: "List",
   component: List,
+  argTypes: {
+    data: {
+      options: ["3items", "4items"],
+      mapping: {
+        "3items": dataForList.slice(1),
+        "4items": dataForList.slice(),
+      },
+    },
+  },
 };
 
 export default meta;
@@ -11,16 +27,11 @@ export default meta;
 type Story = StoryObj<typeof List>;
 
 export const NormalList: Story = {
-  render: () => (
-    <List
-      data={[
-        { id: "1", text: "1日後" },
-        { id: "2", text: "2日後" },
-        { id: "3", text: "3日後" },
-        { id: "4", text: "4日後" },
-      ]}
-      onDelete={(id) => console.log(`Delete item ${id}`)}
-      onUpdate={(id) => console.log(`Update item ${id}`)}
-    />
-  ),
+  args: {
+    data: [
+      { id: "1", text: "1日後" },
+      { id: "2", text: "2日後" },
+      { id: "3", text: "3日後" },
+    ],
+  },
 };
