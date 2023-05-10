093bba3f5ebf6ab755bfd8d584b17eeb74d2923a
awyaki <awyakitaka@gmail.com>
2023-05-10 17:56:53 +0900

    chore: make the view of Menu page

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,39 @@
+import { NextPage } from "next";
+import { MenuItem } from "./components";
+
+const Page: NextPage = () => {
+  const menuItems = [
+    "IDを発行する",
+    "発行済みのIDを見る",
+    "通知スケジュールのプリセットを作成する",
+  ];
+
+  return (
+    <article className="h-screen bg-sky">
+      <h1 className="mb-4 text-xl text-white">Menu</h1>
+      <ul className="mb-10">
+        {menuItems.map((text, i, items) => {
+          const _rounded = (() => {
+            if (i === 0) return "top";
+            if (i === items.length - 1) return "bottom";
+            return "none";
+          })();
+
+          return (
+            <MenuItem
+              key={text}
+              text={text}
+              rounded={_rounded}
+              color={i % 2 === 0 ? "light-gray" : "gray"}
+            />
+          );
+        })}
+      </ul>
+      <ul>
+        <li className="p-4 rounded-t-lg rounded-b-lg bg-light-gray">設定</li>
+      </ul>
+    </article>
+  );
+};
+
+export default Page;
