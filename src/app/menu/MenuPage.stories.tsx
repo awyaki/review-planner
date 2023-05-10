826421b4c3cff996210c2717257b9ff738a07bf8
awyaki <awyakitaka@gmail.com>
2023-05-10 22:51:31 +0900

    chore: add story for Menu page

---
+++ src/app/menu/MenuPage.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import MenuPage from "./page";
+
+const meta: Meta<typeof MenuPage> = {
+  title: "MenuPage",
+  component: MenuPage,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof MenuPage>;
+
+export const Primary: Story = {
+  render: () => <MenuPage />,
+};
