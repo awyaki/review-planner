2e0c6e46667f93fc339f5a1bd5a6c54985bc238d
awyaki <awyakitaka@gmail.com>
2023-05-11 15:19:34 +0900

    feat: add back button on Menu page

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,7 +1,12 @@
+"use client";
 import { NextPage } from "next";
+import { useRouter } from "next/navigation";
 import { MenuItem } from "./components";
+import { AiOutlineLeft } from "react-icons/ai";
 
 const Page: NextPage = () => {
+  const router = useRouter();
+
   const menuItems = [
     "IDを発行する",
     "発行済みのIDを見る",
@@ -10,6 +15,15 @@ const Page: NextPage = () => {
 
   return (
     <article className="h-screen p-5 bg-sky">
+      <header className="pt-2 pb-6">
+        <button
+          className="flex mb-2 text-white items-center"
+          onClick={() => router.back()}
+        >
+          <AiOutlineLeft className="mr-1" />
+          <span>戻る</span>
+        </button>
+      </header>
       <h1 className="mb-4 text-xl text-white">Menu</h1>
       <ul className="mb-10">
         {menuItems.map((text, i, items) => {
