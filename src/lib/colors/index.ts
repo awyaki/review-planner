42d2268e3f71f861d03ab91ee84f37bdb9708da7
awyaki <awyakitaka@gmail.com>
2023-05-11 19:59:54 +0900

    chore: add colors

--- src/lib/colors/index.ts
+++ src/lib/colors/index.ts
@@ -1,1 +1,13 @@
 export type Colors = "sky" | "white" | "gray" | "light-gray" | "dark-gray";
+
+/*
+ * This colors object must be
+ * sync to confing.theme.colors in tailwind.config.js
+ **/
+export const colors = {
+  sky: "#72B2E6",
+  white: "#FFFFFF",
+  gray: "#D8D8D8",
+  "light-gray": "#F1F1F1",
+  "dark-gray": "#616161",
+};
