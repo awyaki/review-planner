e724440d46fb84f0c090f3c94e805296c1b6098a
awyaki <awyakitaka@gmail.com>
2023-05-11 19:59:07 +0900

    chore: move Colors type to index.ts from types.ts

--- src/lib/colors/index.ts
+++ src/lib/colors/index.ts
@@ -1,1 +1,1 @@
-export * from "./types";
+export type Colors = "sky" | "white" | "gray" | "light-gray" | "dark-gray";
--- src/lib/colors/types.ts
+++
@@ -1,1 +0,0 @@
-export type Colors = "sky" | "white" | "gray" | "light-gray" | "dark-gray";
