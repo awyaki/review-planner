8329ca91967cb1609f40c89520d9dc8c7c1edbd2
awyaki <awyakitaka@gmail.com>
2023-05-14 08:38:06 +0900

    chore: add ThemeColorContextProvider to the story of SettingsPage

--- src/app/settings/SettingsPage.stories.tsx
+++ src/app/settings/SettingsPage.stories.tsx
@@ -1,9 +1,19 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import SettingsPage from "./page";
+import { ThemeColorContextProvider } from "@/providers";
 
 const meta: Meta<typeof SettingsPage> = {
   title: "SettingsPage",
   component: SettingsPage,
+  decorators: [
+    (Story) => {
+      return (
+        <ThemeColorContextProvider>
+          <Story />
+        </ThemeColorContextProvider>
+      );
+    },
+  ],
 };
 
 export default meta;
