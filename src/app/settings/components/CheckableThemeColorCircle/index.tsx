35aea4168d8ccbdfeb656cb856644143416b3a24
awyaki <awyakitaka@gmail.com>
2023-05-13 19:22:33 +0900

    feat: add checking/unchecking animation to CheckableThemeColorCircle

---
+++ src/app/settings/components/CheckableThemeColorCircle/index.tsx
@@ -0,0 +1,40 @@
+import { type Colors, colors } from "@/lib/colors";
+import { motion } from "framer-motion";
+
+type Props = {
+  isChecked: boolean;
+  background: Colors;
+  theme: Colors;
+};
+
+const CheckableThemeColorCircle: React.FC<Props> = ({
+  isChecked,
+  background,
+  theme,
+}) => {
+  const size = 36;
+  const center = {
+    x: size / 2,
+    y: size / 2,
+  };
+
+  const r = size / 2;
+
+  return (
+    <svg width={size} height={size}>
+      <circle fill={colors[background]} cx={center.x} cy={center.y} r={r} />
+      <circle fill={colors[theme]} cx={center.x} cy={center.y} r={r - 3} />
+      <motion.path
+        fill="none"
+        strokeWidth={2}
+        stroke={colors[background]}
+        initial={{ pathLength: 0 }}
+        animate={{ pathLength: isChecked ? 1 : 0 }}
+        transition={{ duration: 0.5 }}
+        d="M 9,19 l 7 6 l 10 -14"
+      />
+    </svg>
+  );
+};
+
+export { CheckableThemeColorCircle };
