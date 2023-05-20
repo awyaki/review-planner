803be6957b973b0e49a7dc55f36ee8c6eec17576
awyaki <awyakitaka@gmail.com>
2023-05-20 17:38:28 +0900

    chore: restructure Color and Theme types

--- src/lib/colors/index.ts
+++ src/lib/colors/index.ts
@@ -1,3 +1,8 @@
+type Color = {
+  code: string;
+  rgb: string;
+};
+
 export const colors = {
   sky: { code: "#72B2E6", rgb: "114 178 230" },
   white: { code: "#FFFFFF", rgb: "255 255 255" },
@@ -6,13 +11,33 @@ export const colors = {
   orange: { code: "#FF8C45", rgb: "255 140 69" },
   "light-gray": { code: "#F1F1F1", rgb: "241 241 241" },
   "dark-gray": { code: "#616161", rgb: "97 97 97" },
-};
+} satisfies { [k: string]: Color };
 
 export type Colors = keyof typeof colors;
 
-export const themes = [
-  { primary: "sky", background: "white" },
-  { primary: "orange", background: "white" },
-] as const;
+export type Theme = {
+  primary: Color;
+  "bg-primary": Color;
+  "bg-secondary": Color;
+  "text-on-bg-primary": Color;
+  "text-on-bg-secondary": Color;
+};
 
-export type Theme = FlatArray<typeof themes, 1>;
+export const themes = {
+  "sky-light": {
+    primary: { ...colors.sky },
+    "bg-primary": { ...colors.white },
+    "bg-secondary": { ...colors.sky },
+    "text-on-bg-primary": { ...colors["dark-gray"] },
+    "text-on-bg-secondary": { ...colors.white },
+  },
+  "orange-light": {
+    primary: { ...colors.orange },
+    "bg-primary": { ...colors.white },
+    "bg-secondary": { ...colors.orange },
+    "text-on-bg-primary": { ...colors["dark-gray"] },
+    "text-on-bg-secondary": { ...colors.white },
+  },
+} satisfies { [k: string]: Theme };
+
+export type ThemeName = keyof typeof themes;
