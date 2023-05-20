1586a36ac06d0d878cdf6d0e96214f1dc8486792
awyaki <awyakitaka@gmail.com>
2023-05-20 19:09:05 +0900

    fix: fix props values

--- src/app/settings/components/ThemeColorCircle/ThemeColorCircle.stories.tsx
+++ src/app/settings/components/ThemeColorCircle/ThemeColorCircle.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { ThemeColorCircle } from "./index";
+import { colors } from "@/lib/colors";
 
 const meta: Meta<typeof ThemeColorCircle> = {
   title: "ThemeColorCircle",
@@ -11,5 +12,10 @@ export default meta;
 type Story = StoryObj<typeof ThemeColorCircle>;
 
 export const Primary: Story = {
-  render: () => <ThemeColorCircle background="white" theme="sky" />,
+  render: () => (
+    <ThemeColorCircle
+      background={colors["white"].code}
+      color={colors["orange"].code}
+    />
+  ),
 };
