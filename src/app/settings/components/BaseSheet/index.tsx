aff82653e475c2d148a25d9eaaf9931caf664cdc
awyaki <awyakitaka@gmail.com>
2023-05-21 10:41:17 +0900

    chore: add BaseSheet

--- src/app/settings/components/BaseSheet/index.tsx
+++ src/app/settings/components/BaseSheet/index.tsx
@@ -8,26 +8,43 @@ const BaseSheet: React.FC<Props> = ({ onClose }) => {
   return (
     <Sheet onClose={onClose}>
       <h2 className="mb-5 text-lg">IDの表示形式</h2>
-      <BaseSelectButton isSelected text="16進法" />
+      <ul>
+        <li className="mb-3">
+          <BaseSelectButton
+            isSelected={true}
+            text="十進法"
+            onClick={() => {}}
+          />
+        </li>
+        <li>
+          <BaseSelectButton
+            isSelected={false}
+            text="十六進法"
+            onClick={() => {}}
+          />
+        </li>
+      </ul>
     </Sheet>
   );
 };
 
 type BaseSelectButtonProps = {
   isSelected: boolean;
+  onClick: () => void;
   text: string;
 };
 
 const BaseSelectButton: React.FC<BaseSelectButtonProps> = ({
+  onClick,
   isSelected,
   text,
 }) => {
   const variants = isSelected
-    ? "flex items-center justify-between w-full px-5 py-2 rounded-md  bg-bg-secondary text-text-on-bg-secondary border border-primary"
-    : "flex items-center justify-between w-full px-5 py-2 rounded-md  border-bg-secondary border";
+    ? "flex items-center justify-between w-full px-5 py-2 rounded-md  bg-primary text-text-on-primary border border-primary"
+    : "flex items-center justify-between w-full px-5 py-2 rounded-md  border-primary border";
   return (
-    <button className={variants}>
-      <div className="">{text}</div>
+    <button className={variants} onClick={onClick}>
+      <div>{text}</div>
       {isSelected && <div className="text-xs">選択中</div>}
     </button>
   );
