8d55797df4a927416aea2a4443a3cc5d3a84fc7c
awyaki <awyakitaka@gmail.com>
2023-05-25 14:13:37 +0900

    chore: make publish id page

---
+++ src/app/publishid/PublishId.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import PublishId from "./page";
+import { ThemeColorContextProvider, BaseContextProvider } from "@/providers";
+
+const meta: Meta<typeof PublishId> = {
+  title: "PublishId",
+  component: PublishId,
+  decorators: [
+    (Story) => {
+      return (
+        <ThemeColorContextProvider>
+          <BaseContextProvider>
+            <Story />
+          </BaseContextProvider>
+        </ThemeColorContextProvider>
+      );
+    },
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof PublishId>;
+
+export const Primary: Story = {
+  render: () => <PublishId />,
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+};
---
+++ src/app/publishid/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { useState } from "react";
+import { NextPage } from "next";
+import { useRouter } from "next/navigation";
+import { AiOutlineLeft } from "react-icons/ai";
+import Link from "next/link";
+import { List, SmallButton, LargeButton } from "@/components";
+import { EmptyScheduleItem } from "./components";
+import { useAddOneNotificationSheet } from "./hooks";
+
+const Page: NextPage = () => {
+  const router = useRouter();
+  const [schedule, setSchedule] = useState<number[]>([1, 3, 5]);
+  const [render, handleOpen] = useAddOneNotificationSheet();
+  return (
+    <>
+      {render()}
+      <article className="h-screen bg-bg-primary text-text-on-bg-primary">
+        <div className="px-5 pt-5">
+          <header className="flex items-center justify-between mb-5">
+            <button
+              className="flex items-center text-primary"
+              onClick={() => router.back()}
+            >
+              <AiOutlineLeft className="mr-1" />
+              <span>戻る</span>
+            </button>
+            <Link href="/menu">
+              <SmallButton text="メニュー" />
+            </Link>
+          </header>
+          <h1 className="mb-4 text-xl">新規IDの発行</h1>
+          <div className="mb-8">
+            {schedule.length === 0 ? (
+              <ul>
+                <li>
+                  <EmptyScheduleItem />
+                </li>
+              </ul>
+            ) : (
+              <List
+                data={schedule.map((i) => ({ id: `${i}`, text: `${i}日後` }))}
+              ></List>
+            )}
+          </div>
+          <SmallButton text="通知を追加" onClick={handleOpen} />
+        </div>
+        <div className="absolute bottom-0 left-0 w-full">
+          <LargeButton text="IDを発行" onClick={() => {}} />
+        </div>
+      </article>
+    </>
+  );
+};
+
+export default Page;
