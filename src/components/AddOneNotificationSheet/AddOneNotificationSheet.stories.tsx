b4b4f004a6214021a4b1f441c30a9b9eeb317063
awyaki <awyakitaka@gmail.com>
2023-05-26 10:44:02 +0900

    chore: add story of AddOneNotificationSheet

---
+++ src/components/AddOneNotificationSheet/AddOneNotificationSheet.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { AddOneNotificationSheet } from "./index";
+import { ThemeColorContextProvider } from "@/providers";
+
+const meta: Meta<typeof AddOneNotificationSheet> = {
+  title: "AddOneNotificationSheet",
+  component: AddOneNotificationSheet,
+  decorators: [
+    (Story) => (
+      <ThemeColorContextProvider>
+        <Story />
+      </ThemeColorContextProvider>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof AddOneNotificationSheet>;
+
+export const Primary: Story = {
+  render: () => <AddOneNotificationSheet onClose={() => {}} />,
+};
