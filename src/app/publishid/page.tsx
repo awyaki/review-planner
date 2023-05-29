2b65206d16bb88d3aa4fcb837183a866f6309657
awyaki <awyakitaka@gmail.com>
2023-05-29 14:15:11 +0900

    chore: add SelectPresetSheet on publish id page

--- src/app/publishid/page.tsx
+++ src/app/publishid/page.tsx
@@ -8,18 +8,22 @@ import Link from "next/link";
 import { List, SmallButton, LargeButton } from "@/components";
 import { EmptyScheduleItem } from "./components";
 import { useAddOneNotificationSheet } from "@/hooks";
+import { useSelectPresetSheet } from "./hooks";
 
 const Page: NextPage = () => {
   const router = useRouter();
   const [schedule, setSchedule] = useState<number[]>([1, 3, 5]);
   const [renderAddOneNotificationSheet, handleOpenAddOneNotificationSheet] =
     useAddOneNotificationSheet();
+  const [renderSelectPresetSheet, handleOpenSelectPresetSheet] =
+    useSelectPresetSheet();
   const { base } = useContext(BaseContext);
 
   const NEXT_ID = 1234;
   return (
     <>
       {renderAddOneNotificationSheet()}
+      {renderSelectPresetSheet()}
       <article className="h-screen bg-bg-primary text-text-on-bg-primary">
         <h1 className="invisible">新規IDの作成</h1>
         <section className="px-5 pt-5">
@@ -45,7 +49,10 @@ const Page: NextPage = () => {
           </div>
           <div className="flex items-center justify-between mb-4">
             <h2 className="text-xl">通知スケジュール</h2>
-            <button className="px-2 py-1 rounded-lg bg-primary text-text-on-primary">
+            <button
+              className="px-2 py-1 rounded-lg bg-primary text-text-on-primary"
+              onClick={handleOpenSelectPresetSheet}
+            >
               プリセットから選択
             </button>
           </div>
