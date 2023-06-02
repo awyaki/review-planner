29270d65dbf9d8eda8537f56a8b0aee8c626db89
awyaki <awyakitaka@gmail.com>
2023-06-02 15:10:51 +0900

    chore: export ClickableList

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./LargeButton";
 export * from "./SmallButton";
 export * from "./list/List";
+export * from "./list/ClickableList";
 export * from "./Sheet";
 export * from "./AddOneNotificationSheet";
 export * from "./SelectPresetSheet";
