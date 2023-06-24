bd3b58bee33cdb03db66277a220243faaa9fc4ca
awyaki <awyakitaka@gmail.com>
2023-06-24 14:59:33 +0900

    chore: add input area to be filled in base date

--- src/components/SelectPresetSheet/index.tsx
+++ src/components/SelectPresetSheet/index.tsx
@@ -9,11 +9,20 @@ type Props = {
 export const SelectPresetSheet: React.FC<Props> = ({ onClose }) => {
   const presets = ["単語用", "長期用", "短め"];
   const [selected, setSelected] = useState("単語用");
-
+  const [baseDate, setBaseDate] = useState(getYearMonthDay(new Date()));
   return (
     <Sheet color="reverse" onClose={onClose}>
       <div className="px-5">
         <h2 className="mb-5 text-lg">通知プリセットを選択</h2>
+        <div className="flex">
+          <span>基準：</span>
+          <input
+            className="px-1 mb-3 rounded-sm text-dark-gray"
+            type="date"
+            onChange={(e) => setBaseDate(e.target.value)}
+            value={baseDate}
+          />
+        </div>
         <ul>
           {presets.map((item, i) => (
             <li key={item} className={i === presets.length - 1 ? "" : "mb-3"}>
@@ -61,4 +70,18 @@ const SelectPresetButton: React.FC<SelectPresetButtonProps> = ({
       {isSelected && <div className="text-xs">選択中</div>}
     </button>
   );
+};
+
+const getYearMonthDay = (date: Date): string => {
+  const y = `${date.getFullYear()}`;
+  const m = (() => {
+    const m = date.getMonth() + 1;
+    return m >= 10 ? `${m}` : `0${m}`;
+  })();
+  const d = (() => {
+    const d = date.getDate();
+    return d >= 10 ? `${d}` : `0${d}`;
+  })();
+
+  return `${y}-${m}-${d}`;
 };
