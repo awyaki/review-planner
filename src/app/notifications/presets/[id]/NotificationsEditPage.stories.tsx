b9daa213730c92968ab3d6562a6b35c40a0ed709
awyaki <awyakitaka@gmail.com>
2023-06-26 10:57:32 +0900

    chore: add story for NotificationsEditPage

---
+++ src/app/notifications/presets/[id]/NotificationsEditPage.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import NotificationsEdit from "./page";
+import { ThemeColorContextProvider, BaseContextProvider } from "@/providers";
+
+const meta: Meta<typeof NotificationsEdit> = {
+  title: "NotificationsEdit",
+  component: NotificationsEdit,
+  decorators: [
+    (Story) => {
+      return (
+        <ThemeColorContextProvider>
+          <BaseContextProvider>
+            <Story />
+          </BaseContextProvider>
+        </ThemeColorContextProvider>
+      );
+    },
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof NotificationsEdit>;
+
+export const Primary: Story = {
+  render: () => <NotificationsEdit params={{ id: "英単語用" }} />,
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+};
