53df6255ec06b44f0d6ce987cc9ef30e363a7392
awyaki <awyakitaka@gmail.com>
2023-07-14 21:20:01 +0900

    chore: make Header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { AiOutlineLeft } from "react-icons/ai";
+import { useRouter } from "next/navigation";
+export const Header = () => {
+  const router = useRouter();
+  return (
+    <header className="pt-2 pb-6">
+      <button className="flex items-center mb-2" onClick={() => router.back()}>
+        <AiOutlineLeft className="mr-1" />
+        <span>戻る</span>
+      </button>
+    </header>
+  );
+};
