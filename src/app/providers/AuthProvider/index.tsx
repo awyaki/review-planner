2647d6f639d7b8ce76cbe4e5acbb95effc57d349
awyaki <awyakitaka@gmail.com>
2023-07-17 09:39:45 +0900

    chore: replace with GoogleOneTap component

--- src/app/providers/AuthProvider/index.tsx
+++ src/app/providers/AuthProvider/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { createContext } from "react";
 import { useSessionUser } from "./hooks";
-import Script from "next/script";
+import { GoogleOneTap } from "@/app/components";
 
 export const AuthContext = createContext<{
   user: { id: string; name: string } | null;
@@ -15,19 +15,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     <AuthContext.Provider value={{ user: data ?? null }}>
       {!data ? (
         <>
-          <Script
-            src="https://accounts.google.com/gsi/client"
-            async
-            defer
-          ></Script>
-          <div
-            id="g_id_onload"
-            data-client_id={process.env.NEXT_PUBLIC_GOOGLE_ID}
-            data-context="signin"
-            data-login_uri="http://localhost:3000/api/login"
-            data-auto_select="true"
-            data-itp_support="true"
-          ></div>
+          <GoogleOneTap />
           <article className="h-screen p-5 bg-bg-secondary text-text-on-bg-secondary">
             <div
               className="g_id_signin"
