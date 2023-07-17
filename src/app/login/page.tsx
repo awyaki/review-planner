3bfa1b7f2a50ca77e991b3b42bd725318cc61676
awyaki <awyakitaka@gmail.com>
2023-07-17 11:23:52 +0900

    refactor: extract LoginWithGoogleButton from Login Page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,18 +6,24 @@ const Page = () => {
       <GoogleOneTap />
       <article className="h-screen p-5 bg-bg-secondary text-text-on-bg-secondary">
         <h1 className="mb-4 text-xl">ログイン</h1>
-        <div
-          className="g_id_signin"
-          data-type="standard"
-          data-size="large"
-          data-theme="outline"
-          data-text="sign_in_with"
-          data-shape="rectangular"
-          data-logo_alignment="left"
-        ></div>
+        <LoginWithGoogleButton />
       </article>
     </>
   );
 };
 
 export default Page;
+
+const LoginWithGoogleButton = () => {
+  return (
+    <div
+      className="g_id_signin"
+      data-type="standard"
+      data-size="large"
+      data-theme="outline"
+      data-text="sign_in_with"
+      data-shape="rectangular"
+      data-logo_alignment="left"
+    ></div>
+  );
+};
