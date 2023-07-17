862ac3fdda4b6fd1856cc920ea8be028e4fbc095
awyaki <awyakitaka@gmail.com>
2023-07-17 11:02:20 +0900

    chore: quit using AuthContext.Provider on /login page

--- src/app/providers/AuthProvider/index.tsx
+++ src/app/providers/AuthProvider/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { createContext } from "react";
 import { useSessionUser } from "./hooks";
-import { redirect } from "next/navigation";
+import { redirect, usePathname } from "next/navigation";
 
 export const AuthContext = createContext<{
   user: { id: string; name: string } | null;
@@ -9,8 +9,12 @@ export const AuthContext = createContext<{
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const { data, isLoading } = useSessionUser();
-  // TODO: replace with Loading component
-  if (isLoading) return <>loading...</>;
+  const path = usePathname();
+  if (path === "/login") return <>{children}</>;
+
+  if (isLoading)
+    // TODO: replace with Loading component
+    return <>loading...</>;
   if (!data) {
     redirect("http://localhost:3000/login");
   }
