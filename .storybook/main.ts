241fc6bf67b136bb131ab3259150f4a81b09b99a
awyaki <awyakitaka@gmail.com>
2023-07-20 14:00:35 +0900

    fix: add path ailias to src/lib as @/lib

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -20,6 +20,7 @@ const config: StorybookConfig = {
       ...config.resolve.alias,
       "@/app": path.resolve(__dirname, "../src/app"),
       "@/types": path.resolve(__dirname, "../src/types"),
+      "@/lib": path.resolve(__dirname, "../src/lib"),
       "@/lib/colors": path.resolve(__dirname, "../src/lib/colors"),
       "@/components": path.resolve(__dirname, "../src/components"),
       "@/providers": path.resolve(__dirname, "../src/providers"),
