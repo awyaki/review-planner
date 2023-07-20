3a83c14c9fea93cb57c1d9e3fd10151981c0fd3e
awyaki <awyakitaka@gmail.com>
2023-07-20 15:04:27 +0900

    change: replace a letter with white rectangle

--- src/app/components/Skeleton/HeaderSkeleton/index.tsx
+++ src/app/components/Skeleton/HeaderSkeleton/index.tsx
@@ -4,7 +4,7 @@ export const HeaderSkeleton = () => {
     <header className="pt-2 pb-6">
       <div className="flex items-center mb-2">
         <AiOutlineLeft className="mr-1" />
-        <span>戻る</span>
+        <div className="w-12 p-2 bg-text-on-bg-secondary rounded-md"></div>
       </div>
     </header>
   );
