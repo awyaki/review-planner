2db291fbed910eda85b3d738e777044852380181
awyaki <awyakitaka@gmail.com>
2023-07-20 15:08:50 +0900

    chore: add MenuSkeleton to Skeleton component

--- src/app/components/Skeleton/index.tsx
+++ src/app/components/Skeleton/index.tsx
@@ -1,1 +1,16 @@
-export const Skeleton = () => {};
+import { MenuSkeleton } from "./MenuSkeleton";
+
+type Props = {
+  path: string;
+};
+
+export const Skeleton: React.FC<Props> = ({ path }) => {
+  switch (path) {
+    case "/menu": {
+      return <MenuSkeleton />;
+    }
+    default: {
+      return <>Loading...</>;
+    }
+  }
+};
