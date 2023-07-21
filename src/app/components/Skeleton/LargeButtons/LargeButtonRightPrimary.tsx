1aabb4468cab443e5aa9a221e4860a547426cf90
awyaki <awyakitaka@gmail.com>
2023-07-21 15:09:29 +0900

    chore: make LargeButtonRightPrimary

---
+++ src/app/components/Skeleton/LargeButtons/LargeButtonRightPrimary.tsx
@@ -0,0 +1,3 @@
+export const LargeButtonRightPrimary = () => {
+  return <div className="w-1/2 py-9 bg-bg-secondary rounded-tr-xl"></div>;
+};
--- src/app/components/Skeleton/LargeButtons/index.ts
+++ src/app/components/Skeleton/LargeButtons/index.ts
@@ -1,1 +1,2 @@
 export * from "./LargeButtonNormalLeft";
+export * from "./LargeButtonRightPrimary";
