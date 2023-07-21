3692ecff40165d3cb4bfe016be80fcdad00f5dbf
awyaki <awyakitaka@gmail.com>
2023-07-21 10:09:08 +0900

    chore: add skeleton for idinfo page

--- src/app/components/Skeleton/index.tsx
+++ src/app/components/Skeleton/index.tsx
@@ -1,17 +1,24 @@
 import { MenuSkeleton } from "./MenuSkeleton";
 import { SettingSkeleton } from "./SettingSkeleton";
+import { IdInfoSkeleton } from "./IdInfoSkeleton";
 
 type Props = {
   path: string;
 };
 
 export const Skeleton: React.FC<Props> = ({ path }) => {
-  switch (path) {
-    case "/menu": {
+  // ex. when path is /idinfo/125, path.split("/") will ["", "idinfo", "125"]
+  const splitted = path.split("/")[1];
+
+  switch (splitted) {
+    case "menu": {
       return <MenuSkeleton />;
     }
-    case "/settings": {
+    case "settings": {
       return <SettingSkeleton />;
+    }
+    case "idinfo": {
+      return <IdInfoSkeleton />;
     }
     default: {
       return <>Loading...</>;
