074fdef7d575f07c4a44267bcce47928f83a0b7e
awyaki <awyakitaka@gmail.com>
2023-07-22 17:15:48 +0900

    chore: quit redirect

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -3,13 +3,11 @@ import { getSession } from "@/lib/session";
 
 export async function POST(request: NextRequest) {
   try {
-    const response = new Response(null, {
-      status: 302,
-      headers: { Location: `http://localhost:3000/menu` },
-    });
+    const response = new Response();
 
     const session = await getSession(request, response);
     await session.destroy();
+    return response;
   } catch (e) {
     throw e;
   }
