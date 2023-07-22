4a30e7bd3b283e810d3dd3bba4379122f31e94bf
awyaki <awyakitaka@gmail.com>
2023-07-22 09:47:12 +0900

    change: provide SessionUser | null type value from AuthProvider

--- src/app/providers/AuthProvider/index.tsx
+++ src/app/providers/AuthProvider/index.tsx
@@ -3,10 +3,9 @@ import { createContext } from "react";
 import { useSessionUser } from "./hooks";
 import { redirect, usePathname } from "next/navigation";
 import { Skeleton } from "@/app/components";
+import { SessionUser } from "@/types";
 
-export const AuthContext = createContext<{
-  user: { id: string; name: string } | null;
-}>({ user: null });
+export const AuthContext = createContext<SessionUser | null>(null);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const { data, isLoading } = useSessionUser();
@@ -17,9 +16,5 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   if (!data) {
     redirect("http://localhost:3000/login");
   }
-  return (
-    <AuthContext.Provider value={{ user: data ?? null }}>
-      {children}
-    </AuthContext.Provider>
-  );
+  return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
 };
