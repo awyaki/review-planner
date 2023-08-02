7b1a27e7365d8ae0376b4543eb77d4ecf57ce29d
awyaki <awyakitaka@gmail.com>
2023-08-02 20:58:09 +0900

    chore: add NextID object store

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,13 +7,18 @@ export type ID = {
   notifications: Notification[];
 };
 
+export type NextID = number;
+
 export class MySubClassedDexie extends Dexie {
   ID!: Table<ID>;
+  NextID!: Table<NextID>;
 
   constructor() {
     super("review_planner");
     this.version(1).stores({
       ID: "&id",
+      // assume NextID object store will have only one record whose is `next_id`.
+      NextID: "id",
     });
   }
 }
