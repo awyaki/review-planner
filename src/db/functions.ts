9abc1c4b2bc8a50521653d2630a73d33cdaa5972
awyaki <awyakitaka@gmail.com>
2023-08-02 21:11:08 +0900

    chore: add addId function

--- src/db/functions.ts
+++ src/db/functions.ts
@@ -1,6 +1,13 @@
-import { db } from "./index";
+import { db, Notification } from "./index";
 
 export const fetchNextId = async (): Promise<number> => {
   const nextID = await db.NextID.get("next_id");
   return nextID ?? 0;
+};
+
+export const addId = async (
+  id: number,
+  notifications: Notification[]
+): Promise<void> => {
+  await db.ID.add({ id, notifications });
 };
