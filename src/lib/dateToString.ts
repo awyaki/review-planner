cd389ea1bcb30ca012bc95332836d4cc2a72e904
awyaki <awyakitaka@gmail.com>
2023-08-06 14:42:50 +0900

    fix: plus 1 to the value returned by getMonth

--- src/lib/dateToString.ts
+++ src/lib/dateToString.ts
@@ -1,6 +1,6 @@
 export const dateToString = (date: Date) => {
   const y = date.getFullYear();
-  const m = date.getMonth();
+  const m = date.getMonth() + 1;
   const d = date.getDate();
   return `${y}/${m}/${d}`;
 };
