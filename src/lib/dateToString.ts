04f813568542938c4c8131f8977bceb9eb2bf76b
awyaki <awyakitaka@gmail.com>
2023-08-06 14:39:25 +0900

    fix: fix getDay into getDate
    * getDay return number that represent day of week
    * so in this case getDay isn't suitable

--- src/lib/dateToString.ts
+++ src/lib/dateToString.ts
@@ -1,6 +1,6 @@
 export const dateToString = (date: Date) => {
   const y = date.getFullYear();
   const m = date.getMonth();
-  const d = date.getDay();
+  const d = date.getDate();
   return `${y}/${m}/${d}`;
 };
