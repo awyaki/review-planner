a21b77d9c12f2b8a4db97d4f54393a41a95d6e23
awyaki <awyakitaka@gmail.com>
2023-08-11 10:41:31 +0900

    chore: remove initail example schedule data from schedule state

--- src/app/publishid/page.tsx
+++ src/app/publishid/page.tsx
@@ -20,23 +20,7 @@ import useSWR from "swr";
 
 const Page: NextPage = () => {
   const router = useRouter();
-  const [schedule, setSchedule] = useState<Notification[]>([
-    {
-      id: 1,
-      baseDate: new Date("2023-6-23"),
-      daysAfter: 1,
-    },
-    {
-      id: 2,
-      baseDate: new Date("2023-6-23"),
-      daysAfter: 3,
-    },
-    {
-      id: 3,
-      baseDate: new Date("2023-7-23"),
-      daysAfter: 5,
-    },
-  ]);
+  const [schedule, setSchedule] = useState<Notification[]>([]);
 
   const handleAddNotification = useCallback(
     async (baseDate: Date, daysAfter: number) => {
