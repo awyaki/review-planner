c33f2401693c68c8da28f6b663ae54d5c94b7f30
awyaki <awyakitaka@gmail.com>
2023-08-12 11:41:36 +0900

    chore: change styles of buttons

--- src/app/notifications/create/page.tsx
+++ src/app/notifications/create/page.tsx
@@ -34,48 +34,49 @@ const Page: NextPage = () => {
   return (
     <>
       {render()}
-      <article className="flex flex-col justify-between h-screen bg-bg-primary text-text-on-bg-primary">
-        <div className="px-5 pt-5">
-          <header className="flex items-center justify-between mb-5">
-            <button
-              className="flex items-center text-primary"
-              onClick={() => router.back()}
-            >
-              <AiOutlineLeft className="mr-1" />
-              <span>戻る</span>
-            </button>
-            <Link href="/menu">
-              <SmallButton text="メニュー" />
-            </Link>
-          </header>
-          <input
-            type="text"
-            className="w-full px-3 py-2 mb-5 focus:outline-primary"
-            value={inputValue}
-            placeholder="タイトルを入力"
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-          <div className="mb-10">
-            <List
-              data={notifications.map(({ id, day }) => ({
-                id,
-                text: `${day.toString()}日後`,
-              }))}
-              onDelete={handleDeleteNotification}
-            />
-          </div>
-          <div>
-            <SmallButton onClick={handleOpen} text="通知を追加" />
-          </div>
-        </div>
-        <div className="absolute bottom-0 left-0 w-full">
+      <article className="px-5 pt-5 bg-bg-primary text-text-on-bg-primary">
+        <header className="flex items-center justify-between mb-5">
           <button
-            className="w-1/2 py-5 rounded-tl-md bg-gray text-dark-gray"
+            className="flex items-center text-primary"
             onClick={() => router.back()}
+          >
+            <AiOutlineLeft className="mr-1" />
+            <span>戻る</span>
+          </button>
+          <Link href="/menu">
+            <SmallButton text="メニュー" />
+          </Link>
+        </header>
+        <input
+          type="text"
+          className="w-full px-3 py-2 mb-5 focus:outline-primary"
+          value={inputValue}
+          placeholder="タイトルを入力"
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <div className="mb-10">
+          <List
+            data={notifications.map(({ id, day }) => ({
+              id,
+              text: `${day.toString()}日後`,
+            }))}
+            onDelete={handleDeleteNotification}
+          />
+        </div>
+        <div className="mb-5">
+          <SmallButton onClick={handleOpen} text="通知を追加" />
+        </div>
+        <div className="flex gap-3">
+          <button
+            className="w-1/3 px-2 py-2 rounded-lg bg-gray text-dark-gray"
+            onClick={() => {}}
           >
             キャンセル
           </button>
-          <button className="w-1/2 py-5 rounded-tr-md bg-primary text-text-on-primary">
+          <button
+            className="w-1/3 px-2 py-2 rounded-lg bg-bg-secondary text-text-on-bg-secondary"
+            onClick={() => {}}
+          >
             作成
           </button>
         </div>
