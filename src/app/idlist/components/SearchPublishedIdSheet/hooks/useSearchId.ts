6ec5327502d8e23c0cf017a4a52b7669feac4cfd
awyaki <awyakitaka@gmail.com>
2023-08-21 14:50:13 +0900

    chore: filter by min, max and query

--- src/app/idlist/components/SearchPublishedIdSheet/hooks/useSearchId.ts
+++ src/app/idlist/components/SearchPublishedIdSheet/hooks/useSearchId.ts
@@ -21,24 +21,75 @@ type Action =
       maxIdQuery: string;
     };
 
+const filterByQuery = (ids: number[], query: string): number[] => {
+  return query === "" ? [...ids] : ids.filter((id) => id === Number(query));
+};
+
+const filterByMinId = (ids: number[], min: string): number[] => {
+  return min === "" ? [...ids] : ids.filter((id) => id >= Number(min));
+};
+
+const filterByMaxId = (ids: number[], max: string): number[] => {
+  return max === "" ? [...ids] : ids.filter((id) => id <= Number(max));
+};
+
+const filterByAll = (
+  ids: number[],
+  query: string,
+  min: string,
+  max: string
+) => {
+  if (query === "" && min === "" && max === "") return [];
+  const filtered1 = filterByQuery(ids, query);
+  const filtered2 = filterByMinId(filtered1, min);
+  const filtered3 = filterByMaxId(filtered2, max);
+  return filtered3;
+};
+
 const reducer: Reducer<State, Action> = (state, action) => {
   switch (action.type) {
     case "changed_query": {
       const nextState = { ...state };
       const nextQuery = action.query;
-      const nextResult =
-        nextQuery === ""
-          ? []
-          : state.ids.filter((id) => id === Number(nextQuery));
+      const nextResult = filterByAll(
+        state.ids,
+        nextQuery,
+        state.minIdQuery,
+        state.maxIdQuery
+      );
       nextState.result = nextResult;
       nextState.query = nextQuery;
       return nextState;
     }
     case "changed_minIdQuery": {
-      return state;
+      const nextState = { ...state };
+      const nextMinIdQuery = action.minIdQuery;
+
+      const nextResult = filterByAll(
+        state.ids,
+        state.query,
+        nextMinIdQuery,
+        state.maxIdQuery
+      );
+
+      nextState.minIdQuery = nextMinIdQuery;
+      nextState.result = nextResult;
+      return nextState;
     }
     case "changed_maxIdQuery": {
-      return state;
+      const nextState = { ...state };
+      const nextMaxIdQuery = action.maxIdQuery;
+
+      const nextResult = filterByAll(
+        state.ids,
+        state.query,
+        state.minIdQuery,
+        nextMaxIdQuery
+      );
+
+      nextState.maxIdQuery = nextMaxIdQuery;
+      nextState.result = nextResult;
+      return nextState;
     }
     default: {
       return { ...state };
