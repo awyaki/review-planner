67c0f5d67509577b1582ec798146cf8bfda3ace6
awyaki <awyakitaka@gmail.com>
2023-09-04 10:11:33 +0900

    chore: create getAllIds function

--- src/db/functions/id.ts
+++ src/db/functions/id.ts
@@ -1,3 +1,8 @@
 import { db } from "../index";
 
 export const createId = async () => db.id.add({});
+
+export const getAllIds = async () => {
+  const res = await db.id.toArray();
+  return res;
+};
