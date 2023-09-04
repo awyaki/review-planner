98b4f5b1bc9ace5ccd2444ec029b566292c8dc55
awyaki <awyakitaka@gmail.com>
2023-09-04 14:46:46 +0900

    chore: create putOnePreset function

--- src/db/functions/preset.ts
+++ src/db/functions/preset.ts
@@ -1,5 +1,9 @@
 import { db, Preset } from "../index";
-import { parseIntoNumber, NDaysAfterForPresetForClient } from "@/lib";
+import {
+  parseIntoNumber,
+  NDaysAfterForPresetForClient,
+  PresetForClient,
+} from "@/lib";
 
 export const createPreset = async (
   name: string,
@@ -33,6 +37,24 @@ export const getOnePreset = async (id: number): Promise<Preset> => {
     const result = await db.preset.where("id").equals(id).toArray();
     if (result.length === 0) throw NotExistSuchPresetError;
     return result[0];
+  } catch (e) {
+    throw e;
+  }
+};
+
+export const putOnePreset = async (preset: PresetForClient) => {
+  try {
+    await db.transaction("rw", db.preset, db.nDaysAfterForPreset, async () => {
+      const presetId = parseIntoNumber(await db.preset.put(preset));
+      if (presetId instanceof Error) {
+        throw presetId;
+      }
+
+      const nDaysAfters = preset.nDaysAfters;
+      await db.nDaysAfterForPreset.bulkPut(
+        nDaysAfters.map(({ id, n }) => ({ id, n, belongTo: presetId }))
+      );
+    });
   } catch (e) {
     throw e;
   }
