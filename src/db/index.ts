03525d16d3f170a25c5901d87125ab99b64169fe
awyaki <awyakitaka@gmail.com>
2023-09-04 09:50:44 +0900

    chore: add NDaysAfter object store

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,12 +4,21 @@ export type Id = {
   id: number;
 };
 
+export type NDaysAfter = {
+  id: number;
+  belongTo: number;
+  n: number;
+  base: Date;
+};
+
 export class ReviewPlannerDatabase extends Dexie {
   id!: Table<Id>;
+  nDaysAfter!: Table<NDaysAfter>;
   constructor() {
     super("review_planner");
     this.version(1).stores({
       ID: "++id",
+      nDaysAfter: "++id",
     });
   }
 }
