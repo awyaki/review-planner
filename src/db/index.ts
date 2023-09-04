825ef296b679ee2ecd27f38358b15340631b9803
awyaki <awyakitaka@gmail.com>
2023-09-04 11:01:26 +0900

    chore: add type for nDaysAfterForPreset property

--- src/db/index.ts
+++ src/db/index.ts
@@ -26,6 +26,7 @@ export class ReviewPlannerDatabase extends Dexie {
   id!: Table<Id>;
   preset!: Table<Preset>;
   nDaysAfter!: Table<NDaysAfter>;
+  nDaysAfterForPreset!: Table<NDaysAfterForPreset>;
   constructor() {
     super("review_planner");
     this.version(1).stores({
