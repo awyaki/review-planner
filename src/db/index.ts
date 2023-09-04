a55fbdd0bda143c840e42814beef6217e9a45d1a
awyaki <awyakitaka@gmail.com>
2023-09-04 09:47:17 +0900

    chore: remove uncessary code

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,18 +1,4 @@
 import Dexie, { Table } from "dexie";
-/*
-export type DaysAfter = {
-  id: number;
-  baseDate: Date;
-  daysAfter: number;
-};
-*/
-// export type DaysAfterForPreset = Omit<DaysAfter, "baseDate">;
-
-export type Preset = {
-  id?: number;
-  name: string;
-  //notifications: DaysAfterForPreset[];
-};
 
 export type Id = {
   id: number;
@@ -20,12 +6,10 @@ export type Id = {
 
 export class ReviewPlannerDatabase extends Dexie {
   id!: Table<Id>;
-  Preset!: Table<Preset>;
   constructor() {
     super("review_planner");
     this.version(1).stores({
       ID: "++id",
-      Preset: "++id",
     });
   }
 }
