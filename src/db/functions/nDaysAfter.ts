268777ea953842629f1d2af49ac24e2bfa3ad06b
awyaki <awyakitaka@gmail.com>
2023-09-04 10:25:35 +0900

    chore: create getAllNDaysAfter function

--- src/db/functions/nDaysAfter.ts
+++ src/db/functions/nDaysAfter.ts
@@ -1,8 +1,14 @@
-import { db } from "../index";
+import { db, NDaysAfter } from "../index";
+
 export const createNdaysAfter = async (
   n: number,
   belongTo: number,
   base: Date
 ): Promise<void> => {
   await db.nDaysAfter.add({ n, belongTo, base });
+};
+
+export const getAllNDaysAfters = async (): Promise<NDaysAfter[]> => {
+  const res = await db.nDaysAfter.toArray();
+  return res;
 };
