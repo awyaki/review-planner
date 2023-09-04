2dd4dc872e7fd34936cf9ceb1b27d8df5bff9bb7
awyaki <awyakitaka@gmail.com>
2023-09-04 15:18:59 +0900

    chore: create getAllNDaysAftersForPresetOfPresetId function

--- src/db/functions/nDaysAfterForPreset.ts
+++ src/db/functions/nDaysAfterForPreset.ts
@@ -1,8 +1,22 @@
-import { db } from "@/db";
+import { db, NDaysAfterForPreset } from "@/db";
 
 export const createNDaysAfterForPreset = async (
   n: number,
   belongTo: number
 ): Promise<void> => {
   await db.nDaysAfterForPreset.add({ n, belongTo });
+};
+
+export const getAllNDaysAftersForPresetOfPresetId = async (
+  id: number
+): Promise<NDaysAfterForPreset[]> => {
+  try {
+    const res = await db.nDaysAfterForPreset
+      .where("belongTo")
+      .equals(id)
+      .toArray();
+    return res;
+  } catch (e) {
+    throw e;
+  }
 };
