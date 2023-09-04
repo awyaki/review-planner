431de8ef01c8b0d706e3f159afbcd1eacdd2eb2a
awyaki <awyakitaka@gmail.com>
2023-09-04 10:15:22 +0900

    chore: create getCurrentId function

--- src/db/functions/id.ts
+++ src/db/functions/id.ts
@@ -6,3 +6,9 @@ export const getAllIds = async () => {
   const res = await db.id.toArray();
   return res;
 };
+
+export const getCurrentId = async (): Promise<number> => {
+  const ids = await getAllIds();
+  const curMax = ids.reduce((a, b) => Math.max(a, b.id ?? 0), 0);
+  return curMax;
+};
