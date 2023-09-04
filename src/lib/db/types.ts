f697d96576e0747dafa14bb28599ed3e46badfa4
awyaki <awyakitaka@gmail.com>
2023-09-04 14:13:33 +0900

    chore: make PresetForClient type

--- src/lib/db/types.ts
+++ src/lib/db/types.ts
@@ -1,7 +1,9 @@
-import { NDaysAfterForPreset } from "@/db";
+import { NDaysAfterForPreset, Preset } from "@/db";
 
 // data type for client side
 
 export type NDaysAfterForPresetForClient = Required<
   Omit<NDaysAfterForPreset, "belongTo">
 >;
+
+export type PresetForClient = Required<Preset>;
