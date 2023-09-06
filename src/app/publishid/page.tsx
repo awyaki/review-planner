83ee687d41674544049801e0f978498f870543ba
awyaki <awyakitaka@gmail.com>
2023-09-06 13:52:06 +0900

    fix: change number of next id 1 from 0, when current id doesn't exist.

--- src/app/publishid/page.tsx
+++ src/app/publishid/page.tsx
@@ -42,7 +42,7 @@ const Page: NextPage = () => {
           </header>
           <div className="mb-8">
             <span className="block mb-1 text-sm">次のID</span>
-            {isLoading ? <>Loading...</> : <NextId nextId={currentId ?? 0} />}
+            {isLoading ? <>Loading...</> : <NextId nextId={currentId ?? 1} />}
           </div>
           <div className="flex items-center justify-between mb-4">
             <h2 className="text-xl">通知スケジュール</h2>
