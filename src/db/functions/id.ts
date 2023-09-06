77416c833549edd57f1182e66c2f5966e34a9a12
awyaki <awyakitaka@gmail.com>
2023-09-06 14:46:12 +0900

    fix: fix createId function
    * when creating id, create nDaysAfter for the id.

--- src/db/functions/id.ts
+++ src/db/functions/id.ts
@@ -1,6 +1,20 @@
-import { db } from "../index";
+import { parseIntoNumber } from "@/lib";
+import { db, NDaysAfterForClient } from "../index";
 
-export const createId = async () => db.id.add({});
+export const createId = async (nDaysAfters: NDaysAfterForClient[]) => {
+  try {
+    await db.transaction("rw", db.id, db.nDaysAfter, async () => {
+      const _id = await db.id.add({});
+      const id = parseIntoNumber(_id);
+      if (id instanceof Error) throw id;
+      await db.nDaysAfter.bulkAdd(
+        nDaysAfters.map((d) => ({ ...d, belongTo: id }))
+      );
+    });
+  } catch (e) {
+    throw e;
+  }
+};
 
 export const getAllIds = async () => {
   const res = await db.id.toArray();
