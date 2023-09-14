dfbcfc16c0aa42271afe4cc95efe9d5347cd7449
awyaki <awyakitaka@gmail.com>
2023-09-14 10:12:24 +0900

    chore: add constraint validation for input to be filled in nDaysAfter

--- src/app/notifications/components/AddOneNotificationSheetForPreset.tsx
+++ src/app/notifications/components/AddOneNotificationSheetForPreset.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useRef, MouseEventHandler } from "react";
 import { Sheet } from "@/components";
 
 type Props = {
@@ -12,20 +12,34 @@ export const AddOneNotificationSheetForPreset: React.FC<Props> = ({
   onAddNotification,
 }) => {
   const [inputValue, setInputValue] = useState("");
+  const nDaysAfterInput = useRef<HTMLInputElement>(null);
 
-  const handleAddNotification = useCallback(() => {
-    onAddNotification ? onAddNotification(Number(inputValue)) : undefined;
-    onClose();
-  }, [inputValue, onAddNotification, onClose]);
+  const handleAddNotification: MouseEventHandler<HTMLButtonElement> =
+    useCallback(
+      (e) => {
+        e.preventDefault();
+        if (nDaysAfterInput.current) {
+          if (nDaysAfterInput.current.reportValidity()) {
+            onAddNotification
+              ? onAddNotification(Number(inputValue))
+              : undefined;
+            onClose();
+          }
+        }
+      },
+      [inputValue, onAddNotification, onClose, nDaysAfterInput]
+    );
 
   return (
     <Sheet onClose={onClose} color="reverse">
       <div className="flex flex-col justify-between">
-        <div className="px-5 pb-28">
+        <form className="px-5 pb-28">
           <div className="mb-8">
             <input
+              ref={nDaysAfterInput}
               className="w-24 mr-3 bg-light-gray text-dark-gray"
               min={1}
+              required
               type="number"
               value={inputValue}
               onChange={(e) => setInputValue(e.target.value)}
@@ -41,14 +55,13 @@ export const AddOneNotificationSheetForPreset: React.FC<Props> = ({
               キャンセル
             </button>
             <button
-              type="button"
               className="w-1/3 px-2 py-2 rounded-lg bg-bg-primary text-primary"
               onClick={handleAddNotification}
             >
               追加
             </button>
           </div>
-        </div>
+        </form>
       </div>
     </Sheet>
   );
