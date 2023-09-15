02731f210d57800025561dfcbce5ea1dc5c19cb1
awyaki <awyakitaka@gmail.com>
2023-09-15 10:40:40 +0900

    chore: get nDaysAfters on review page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+"use client";
+import { filterTodaysNDaysAfters } from "@/db";
+import useSWR from "swr";
+
 const Page = () => {
-  return <h1 className="text-sky">Hello Next.js by using app directry.</h1>;
+  const { data } = useSWR("/filterNDaysAfters", filterTodaysNDaysAfters);
+  return (
+    <section>
+      <h1>復習ページ</h1>
+      <ul>
+        {data
+          ? data.map(({ belongTo }) => (
+              <li>{`You have to review ${belongTo}`}</li>
+            ))
+          : undefined}
+      </ul>
+    </section>
+  );
 };
 
 export default Page;
