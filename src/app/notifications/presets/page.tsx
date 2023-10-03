2ea37329c35558898745fcd7a421aac7f9b57bd6
awyaki <awyakitaka@gmail.com>
2023-10-03 15:39:45 +0900

    Replace header with HeaderWithMenu

--- src/app/notifications/presets/page.tsx
+++ src/app/notifications/presets/page.tsx
@@ -2,11 +2,10 @@
 import { useCallback } from "react";
 import { type NextPage } from "next";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
 import { SmallButton, ClickableList } from "@/components";
-import { AiOutlineLeft } from "react-icons/ai";
 import { getAllPresets, deleteOnePreset } from "@/db";
 import useSWR from "swr";
+import { HeaderWithMenu } from "@/app/components";
 
 const Page: NextPage = () => {
   const router = useRouter();
@@ -27,19 +26,7 @@ const Page: NextPage = () => {
     <>
       <article className="flex flex-col justify-between h-screen bg-bg-primary text-text-on-bg-primary">
         <div className="px-5 pt-5">
-          <header className="flex items-center justify-between mb-5">
-            <button
-              className="flex items-center text-primary"
-              type="button"
-              onClick={() => router.back()}
-            >
-              <AiOutlineLeft className="mr-1" />
-              <span>戻る</span>
-            </button>
-            <Link href="/menu">
-              <SmallButton text="メニュー" />
-            </Link>
-          </header>
+          <HeaderWithMenu />
           <h2 className="mb-8 text-xl">通知プリセット</h2>
           <div className="mb-8">
             {isLoading ? (
