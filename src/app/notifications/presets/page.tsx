cad74bf945f9675361beeaa8ba7dc07c7390b541
awyaki <awyakitaka@gmail.com>
2023-10-03 15:52:59 +0900

    Add a title metadata for the displaying presets page

--- src/app/notifications/presets/page.tsx
+++ src/app/notifications/presets/page.tsx
@@ -1,7 +1,10 @@
-"use client";
-import { type NextPage } from "next";
+import type { NextPage, Metadata } from "next";
 import { HeaderWithMenu } from "@/app/components";
 import { PresetList, CreatePresetButton } from "./components";
+
+export const metadata: Metadata = {
+  title: "Review Plnner | 通知プリセット",
+};
 
 const Page: NextPage = () => {
   return (
@@ -9,7 +12,7 @@ const Page: NextPage = () => {
       <article className="flex flex-col justify-between h-screen bg-bg-primary text-text-on-bg-primary">
         <div className="px-5 pt-5">
           <HeaderWithMenu />
-          <h2 className="mb-8 text-xl">通知プリセット</h2>
+          <h2 className="mb-8 text-xl">通知プリセットのリストを見る</h2>
           <div className="mb-8">
             <PresetList />
           </div>
